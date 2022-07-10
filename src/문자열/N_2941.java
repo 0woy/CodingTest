acafaa74a14cc0d0ccb301bef8ea88a82038f892
0woy <qkrdbsdk1030@naver.com>
2022-07-10 17:18:16 +0900

    N_2941
    2941번 크로아티아 알파벳

---
+++ src/문자열/N_2941.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class N_2941 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in
+
+        String str = br.readLine();
+
+        String alpha[] = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+        for (int i = 0; i < alpha.length; i++) {
+            if (str.contains(alpha[i])) {
+                str = str.replace(alpha[i], "?");
+            }
+
+        }
+        System.out.print(str.length());
+
+    }
+}
