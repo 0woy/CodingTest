253069c53bcdc089673e4930d1dd245066130989
0woy <qkrdbsdk1030@naver.com>
2022-07-12 00:51:26 +0900

    ACM 호텔
    N_10250 ACM 호텔 풀이

---
+++ src/기본 수학1/N_10250.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N_10250 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int test_case = Integer.parseInt(br.readLine());
+        int h, w, n;
+        int[] num = new int[test_case];
+        int[] floor = new int[test_case];
+        StringTokenizer st;
+        for (int i = 0; i < test_case; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            h = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+
+            if (n % h != 0) {
+                // floor[i] = (n / h) + 1;
+                // num[i] = n % h;
+                floor[i] = n % h;
+                num[i] = (n / h) + 1;
+            } else if (n % h == 0) {
+                floor[i] = h;
+                num[i] = n / h;
+            }
+
+        }
+        for (int i = 0; i < test_case; i++) {
+            System.out.println(floor[i] * 100 + num[i]);
+        }
+
+    }
+}
