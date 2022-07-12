f88dde487eca1da851e5b47a9a97d89eb2b4992d
0woy <qkrdbsdk1030@naver.com>
2022-07-12 17:08:36 +0900

    큰 수 A+B
    N_10757, BigInteger 사용

---
+++ src/기본 수학1/N_10757.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
+
+public class N_10757 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+        a = a.add(b);
+        System.out.println(a.toString());
+    }
+}
