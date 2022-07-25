1b411dffc24b57efa51e972a3858537fa2384c66
0woy <qkrdbsdk1030@naver.com>
2022-07-25 20:30:18 +0900

    Update N_2108_sort.java

--- src/정렬/N_2108_sort.java
+++ src/정렬/N_2108_sort.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class N_2108 {
+public class N_2108_sort {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
