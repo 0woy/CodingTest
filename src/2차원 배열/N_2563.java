74fe0d77a14bfc5bbb23cc3ca47e6cd8ab9305cb
0woy <qkrdbsdk1030@naver.com>
2022-12-22 17:39:27 +0900

    좌표 정렬하기

---
+++ src/2차원 배열/N_2563.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class N_2563 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] box = new box[100][100];
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int[][] rec = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            rec[i][0] = Integer.parseInt(st.nextToken()); // 좌하단 x좌표
+            rec[i][1] = Integer.parseInt(st.nextToken()); // 좌하단 y좌표
+        }
+
+    }
+}
---
+++ src/정렬/N_11650.java
@@ -0,0 +1,48 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class N_11650 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] point = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            point[i][0] = Integer.parseInt(st.nextToken());
+            point[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        // 2차원 배열 정렬 다중조건
+        Arrays.sort(point, new Comparator<int[]>() {
+            @Override
+            // 첫번째 숫자 기준 오름차순 후 같다면 두번째 숫자 기준 오름차순
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] != o2[0] ? o1[0] - o2[0] : o1[1] - o2[1];
+            }
+        });
+
+        /*
+         * ==
+         * Arrays.sort(point, (o1, o2) -> {
+         * if (o1[0] == o2[0])
+         * return o1[1] - o2[1];
+         * else {
+         * return o1[0] - o2[0];
+         * }
+         * });
+         */
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(point[i][0]).append(" ").append(point[i][1]).append('\n');
+            System.out.println(sb.toString());
+        }
+
+    }
+
+}
