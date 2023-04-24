3eb2d4adfe0dd79459be035cf8a51c0b267c6957
0woy <qkrdbsdk1030@naver.com>
2023-04-25 01:06:50 +0900

    큰 수 A+B

--- src/LikeLion_HW/N_10757.java
+++ src/LikeLion_HW/N_10757.java
@@ -1,2 +1,28 @@
-package LikeLion_HW;public class N_10757 {
+/*
+N_10757 큰수 A+B
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+Note.
+BigInter는 java.math내에 있으며 초기화하기 위해선 문자열을 인자 값으로 넘겨줌.
+-> BigInteger가 문자열로 되어있기 때문
+문자열이기 때문에 일반적인 사칙연산이 안되므로 내부 메서드를 사용해야함.
+ex) big1+big2 = big1.add(big2);
+    big*big2 = big1.mulitply(big2);
+ */
+package LikeLion_HW;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class N_10757 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B= new BigInteger(st.nextToken());
+        System.out.println(A.add(B));
+    }
 }
