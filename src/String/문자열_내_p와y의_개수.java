8669c0bdea4ac70d6adc7f984f594d00142dc9af
0woy <qkrdbsdk1030@naver.com>
2024-03-12 15:56:26 +0900

    문자열 내 p와 y의 개수, replace (ft. 효율)

--- src/String/문자열_내_p와y의_개수.java
+++ src/String/문자열_내_p와y의_개수.java
@@ -3,18 +3,10 @@ package String;
 public class 문자열_내_p와y의_개수 {
     class Solution {
         boolean solution(String s) {
-            s = s.toLowerCase();
-            int len = s.length();
-
-            s = s.replace("p", "");
-            int p_len = len-s.length();
-
-            len = s.length();
-
-            s = s.replace("y", "");
-            int y_len = len-s.length();
-
-            return (p_len==y_len)?true:false;
+            s= s.toLowerCase();
+            int p_len = s.length() - s.replace("p","").length();
+            int y_len = s.length() - s.replace("y","").length();
+            return p_len == y_len;
         }
     }
 }
