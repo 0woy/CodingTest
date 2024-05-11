11f72256540d342659c276ff1eeb53dc11d22735
0woy <qkrdbsdk1030@naver.com>
2024-05-11 18:16:42 +0900

    Group Anagrams (ft. map)

--- src/Arrays_Hashing/N_49.java
+++ src/Arrays_Hashing/N_49.java
@@ -1,8 +1,7 @@
 package Arrays_Hashing;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.sql.Array;
+import java.util.*;
 
 /*
 Given an array of strings strs, group the anagrams together.
@@ -22,40 +21,23 @@ Output: [["a"]]
 
  */
 public class N_49 {
-    public static int strToAscii(String str){
-        int sum=0;
-        for(int i=0;i<str.length();i++){
-            sum+=str.charAt(i);
-        }
-        return sum;
+    public static String sortString(String str){
+        char [] charArray = str.toCharArray();
+        Arrays.sort(charArray);
+        return new String(charArray);
+
     }
     public static void main(String[] args) {
-        String[] strs = new String[]{"cab","tin","pew","duh","may","ill","buy","bar","max","doc"};
-        List<Boolean> check = new ArrayList<>();
-        for(String str: strs){
-            check.add(false);
+        String[] strs = new String[]{"eat","tea","tan","ate","nat","bat"};
+        Map<String, List<String>> map = new HashMap<>();
+        for(String str : strs){
+            String sortedStr = sortString(str);
+            if(!map.containsKey(sortedStr)){
+                map.put(sortedStr,new ArrayList<>());
+            }
+            map.get(sortedStr).add(str);
         }
-        List<List<String>> answer = new ArrayList<>();
+       // return new ArrayList<>(map.values());
 
-        for(int i=0;i<strs.length;i++){
-            List<String> list = new ArrayList<>();
-            String flag = strs[i];
-            if(check.get(i)){
-                continue;
-            }
-            list.add(flag);
-            int strInt = strToAscii(flag);
-            check.set(i,true);
-            for(int j=i+1;j<check.size();j++) {
-                // true인 경우 (이미 다른 list에 속한 경우)
-                if (check.get(j)) continue;
-                int fInt = strToAscii(strs[j]);
-                if(strInt==fInt){
-                    check.set(j,true);
-                    list.add(strs[j]);
-                }
-            }
-            answer.add(list);
-        }
     }
 }
