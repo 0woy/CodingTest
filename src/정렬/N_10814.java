002188da3c3e1f0aa58335880e0ae24102c35581
0woy <qkrdbsdk1030@naver.com>
2024-10-19 22:46:06 +0900

    나이순 정렬

--- src/정렬/N_10814.java
+++ src/정렬/N_10814.java
@@ -1,48 +1,39 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class N_10814 {
-
-    public static class Person {
+    static class Person{
         int age;
         String name;
-
-        public Person() {
-            age = 0;
-            name = "";
-        }
-
-        public Person(int age, String name) {
-            this.age = age;
-            this.name = name;
+        public Person(int age, String name){
+            this.age=age;
+            this.name =name;
         }
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        Person[] persons = new Person[N];
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int n= Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        List<Person> people = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            st= new StringTokenizer(br.readLine());
             int age = Integer.parseInt(st.nextToken());
-            String name = st.nextToken();
-            persons[i] = new Person(age, name);
+            String name= st.nextToken();
+            people.add(new Person(age,name));
         }
-        Arrays.sort(persons, new Comparator<Person>() {
+        Collections.sort(people, new Comparator<Person>() {
             @Override
             public int compare(Person o1, Person o2) {
-                if (o1.age == o2.age)
-                    return 0;
-                return o1.age - o2.age;
+                return o1.age-o2.age;
             }
         });
+        for(Person p : people){
+            System.out.println(p.age+" "+p.name);
+        }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++)
-            sb.append(persons[i].age + " " + persons[i].name).append('\n');
 
-        System.out.println(sb);
     }
-
 }
