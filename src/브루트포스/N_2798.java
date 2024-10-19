a8c2ed357205aa33903318b8a1a7417d7701d5ec
0woy <qkrdbsdk1030@naver.com>
2024-10-19 10:19:13 +0900

    블랙잭

--- src/브루트포스/N_2798.java
+++ src/브루트포스/N_2798.java
@@ -3,10 +3,10 @@ import java.io.BufferedReader;
 import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class N_2798 {
+    /** Previous Solve
     static int[] card, numbers;
     static int n, m, res = 0;
 
@@ -46,5 +46,40 @@ public class N_2798 {
             comb(cnt + 1, i + 1);
         }
     }
+     */
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int []  cards = new int[n];
+        st = new StringTokenizer(br.readLine());
+        int idx = 0;
+        while(st.hasMoreTokens()){
+            int card = Integer.parseInt(st.nextToken());
+            if(card >= m){ continue;}
+            cards[idx++] = card;
+        }
+        Arrays.sort(cards);
+        int max =0;
+        for(int i=cards.length-1;i>=2;i--){
+            int fix = cards[i];
+            int right = i-1;
+            for(int left = 0;left<right;){
+                if(fix+cards[left]+cards[right]<=m){
+                   max = Math.max(max, fix+cards[left]+cards[right]);
+                   left++;
+                }
+                else{
+                    if(left == 0){
+                        break;
+                    }
+                    right--;
+                }
+            }
+        }
+        System.out.println(max);
 
+
+    }
 }
