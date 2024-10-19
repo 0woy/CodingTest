a297b13df4252df6264c6b9bbb8f8a895ceeb7fe
0woy <qkrdbsdk1030@naver.com>
2024-10-19 10:49:39 +0900

    단어 정렬

--- src/정렬/N_1181.java
+++ src/정렬/N_1181.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class N_1181 {
+    /** Previous Solve
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -34,5 +35,35 @@ public class N_1181 {
         System.out.println(sb);
 
         br.close();
+    }
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        List<String> list =new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            String str = br.readLine();
+            if(!list.contains(str)){
+                list.add(str);
+            }
+        }
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length()==o2.length()){
+                    return o1.compareTo(o2);
+                }
+                else{
+                    return o1.length()-o2.length();
+                }
+            }
+        });
+        for(String s: list){
+            bw.write(s+"\n");
+        }
+        bw.flush();
+        bw.close();
     }
 }
