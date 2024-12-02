e6f3dc600595bc63cf56d4d83d3dafb7b734ec10
0woy <qkrdbsdk1030@naver.com>
2024-12-02 13:29:17 +0900

    수 정렬하기

--- src/정렬/N_2750.java
+++ src/정렬/N_2750.java
@@ -1,9 +1,9 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class N_2750 {
+    /** Previous Solve
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -18,5 +18,38 @@ public class N_2750 {
             sb.append(i + "\n");
 
         System.out.print(sb);
+    }
+     */
+    /** Priority Queue
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        while(n-- > 0){
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+        while(!pq.isEmpty()){
+            bw.write(pq.poll()+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+     */
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int [] arr= new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for(int v: arr){
+            bw.write(v+"\n");
+        }
+        bw.flush();
+        bw.close();
     }
 }
