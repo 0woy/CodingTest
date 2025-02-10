28b2de4ca7d1c2a3367e18280fe03e3b2cc84537
0woy <qkrdbsdk1030@naver.com>
2025-02-10 12:30:40 +0900

    한 줄로 서기

--- src/Greedy/N_1138.java
+++ src/Greedy/N_1138.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class N_1138 {
+    /** MySolve
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -32,5 +33,26 @@ public class N_1138 {
         bw.flush();
         bw.close();
 
+    }
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] input = new int[n];
+        List<Integer> res = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        for(int i=n;i>0;i--){
+            res.add(input[i-1], i);
+        }
+        for(int v: res){
+            bw.write(v+" ");
+        }
+        bw.flush();
+        bw.close();
     }
 }
