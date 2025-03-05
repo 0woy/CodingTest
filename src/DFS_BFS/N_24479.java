46df08a7f505a3f37b0bbc12413e72159d1419be
0woy <qkrdbsdk1030@naver.com>
2025-03-05 15:24:07 +0900

    알고리즘 수업 - 깊이 우선 탐색 1

--- src/DFS_BFS/N_24479.java
+++ src/DFS_BFS/N_24479.java
@@ -1,12 +1,10 @@
 package DFS_BFS;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class N_24479 {
+    /** Solve with Priority Queue
     static Map<Integer, PriorityQueue<Integer>> map;
     static int [] visited;
     static int idx;
@@ -57,4 +55,56 @@ public class N_24479 {
             }
         }
     }
+     */
+    static List<Integer>[] adj;
+    static int[]visited;
+    static int idx;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(st.nextToken());
+
+        adj = new ArrayList[N + 1];
+        visited = new int[N + 1];
+        idx=1;
+        while (M -- >0){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            if(adj[s]==null)
+                adj[s]= new  ArrayList<>();
+            if(adj[e]==null)
+                adj[e] = new ArrayList<>();
+
+            adj[s].add(e);
+            adj[e].add(s);
+        }
+        for(int i=0;i<adj.length;i++){
+            if(adj[i]!=null){
+                Collections.sort(adj[i]);
+            }
+        }
+
+        dfs(start);
+        for(int i = 1; i <= N; i++){
+            bw.write(visited[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static void dfs(int cur){
+        visited[cur] = idx++;
+        if(adj[cur]==null){return;}
+
+        for(int next: adj[cur]){
+            if(visited[next] == 0){
+                dfs(next);
+            }
+        }
+    }
+
 }
