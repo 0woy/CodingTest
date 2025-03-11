0eeec20598e1146d8531e33bd24a21ee4c7bc130
0woy <qkrdbsdk1030@naver.com>
2025-03-11 15:18:31 +0900

    Koko Eating Bananas

--- src/BinarySearch/N_875.java
+++ src/BinarySearch/N_875.java
@@ -3,6 +3,7 @@ package BinarySearch;
 import java.util.Arrays;
 
 public class N_875 {
+    /** Previous Solve
     public static int validMaxNum;
     public static int res;
     public static long eatBanana(int[] piles, int k){
@@ -26,15 +27,36 @@ public class N_875 {
                 end=half-1;
             }
         }
+    }*/
+    public static int minEatingSpeed(int[] piles, int h) {
+        int start =1;
+        int end = piles[0];
+        for(int i: piles){
+            end = i>end?i:end;
+        }
+        if(piles.length==h){
+            return end;
+        }
+        int res = end;
+        while (start <= end){
+            int eat = start+(end-start)/2;
+            long hour=0;
+            for(int n :piles){
+                hour +=n/eat;
+                if(n%eat > 0){hour++;}
+            }
+            if (hour > h){
+                start = eat+1;
+            }else{
+                res = Math.min(res,eat);
+                end = eat-1;
+            }
+        }
+        return res;
     }
     public static void main(String[] args) {
         int [] piles =new int[]{805306368,805306368,805306368};
-        int h = 1000000000;
-        Arrays.sort(piles);
-        validMaxNum = piles[piles.length-1];
-        res = validMaxNum;
-
-        findK(piles,h,1,validMaxNum);
-        System.out.println(res);
+        int h =1000000000 ;
+        System.out.println(minEatingSpeed(piles,h));
     }
 }
