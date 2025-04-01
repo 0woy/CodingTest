37561e798f7f4dd9edfcd9cbb09f5f1bfee55443
0woy <qkrdbsdk1030@naver.com>
2025-04-01 09:27:08 +0900

    구간 합 구하기 4

--- src/Array/N_11659.java
+++ src/Array/N_11659.java
@@ -1,9 +1,10 @@
-package DP;
+package Array;
 
 import java.io.*;
 import java.util.StringTokenizer;
 
 public class N_11659 {
+    /** Previous Solve
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -25,5 +26,31 @@ public class N_11659 {
         }
         bw.flush();
         bw.close();
+    }
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n= Integer.parseInt(st.nextToken());
+        int tc = Integer.parseInt(st.nextToken());
+        int [] arr = new int[n+1];
+        st = new StringTokenizer(br.readLine());
+        arr[0] = 0;
+
+        for(int i = 1; i <= n; i++){
+            arr[i] = Integer.parseInt(st.nextToken())+arr[i-1];
+        }
+
+        while (tc-- > 0){
+            st= new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            bw.write((arr[e]-arr[s-1])+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+
     }
 }
