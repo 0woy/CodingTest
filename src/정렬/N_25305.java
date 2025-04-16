e877ea6a76d2fd091360dbc17af0dd45c33287f6
0woy <qkrdbsdk1030@naver.com>
2025-04-16 09:50:59 +0900

    커트라인

--- src/정렬/N_25305.java
+++ src/정렬/N_25305.java
@@ -1,3 +1,5 @@
+package 정렬;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,6 +9,7 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class N_25305 {
+    /** Previous Solve ft. Priority Que
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,7 +25,19 @@ public class N_25305 {
             pq.poll();
         }
         System.out.println(pq.poll());
-
-
+    }
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        Integer[] arr =new Integer[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, (a,b) ->b-a);
+        System.out.println(arr[k-1]);
     }
 }
