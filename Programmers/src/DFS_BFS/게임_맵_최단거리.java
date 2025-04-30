c0c4a7873eb003db89999dbe2c7e4a78eb807b10
0woy <qkrdbsdk1030@naver.com>
2025-04-30 16:37:08 +0900

    게임 맵 최단거리

--- Programmers/src/DFS_BFS/게임_맵_최단거리.java
+++ Programmers/src/DFS_BFS/게임_맵_최단거리.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class 게임_맵_최단거리 {
+    /** Previous Solve
     public int solution(int[][] maps) {
         boolean[][] visited= new boolean[maps.length][maps[0].length];
         int answer = bfs(maps, visited, 0,0);
@@ -39,5 +40,42 @@ public class 게임_맵_최단거리 {
         }
         return -1;
     }
+*/
+    static int[] dx =new int[]{0,1,0,-1};
+    static int[] dy =new int[]{1,0,-1,0};
+    public int solution(int[][] maps) {
+        return bfs(maps);
+    }
+    public static int bfs(int[][] maps){
+        Queue<int []> que = new ArrayDeque<>();
+        que.offer(new int[]{0,0});
+        maps[0][0]= -1;
+        int count =1;
+        while(!que.isEmpty()){
+            int len =que.size();
+            count++;
+            while(len -- >0){
+                int [] cur = que.poll();
+                for(int d=0;d<4;d++){
+                    int nx = cur[0]+dx[d];
+                    int ny = cur[1]+dy[d];
 
+                    if(nx == maps.length-1 && ny == maps[0].length-1){
+                        return count;
+                    }
+
+                    if(nx < 0 || ny< 0 || nx >=maps.length || ny>=maps[0].length){
+                        continue;
+                    }
+
+                    if(maps[nx][ny]==1){
+                        que.offer(new int[]{nx,ny});
+                        maps[nx][ny]=-1;
+                    }
+                }
+
+            }
+        }
+        return -1;
+    }
 }
