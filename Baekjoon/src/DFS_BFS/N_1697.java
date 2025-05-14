cd611495a237e0e677b2a0c4e5162e0295a870ff
0woy <qkrdbsdk1030@naver.com>
2025-05-14 15:13:45 +0900

    숨바꼭질

--- Baekjoon/src/DFS_BFS/N_1697.java
+++ Baekjoon/src/DFS_BFS/N_1697.java
@@ -3,47 +3,115 @@ package DFS_BFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+/**
+문제 설명
+수빈이는 위치N에서 시작해 걷기(±1) 또는 순간이동(×2)으로 이동할 수 있습니다.
+동생은 위치 K에 있으며, 수빈이가 동생을 찾는 데 걸리는 최소 시간(초)을 구해야 합니다.
+각 이동은 1초가 걸리며, 0≤N,K≤100,000입니다.
 
+ 시간 복잡도: O(N)
+
+ 풀이
+ 현재 위치 기준 -1, +1, *2 위치로 이동 가능
+ BFS를 순회하면서 동생의 위치와 같게 되는 지점이 가장 최단 거리
+ 동생의 위치는 최대 1,000,000이므로 1,000,001 보다 큰 위치까지 가지 않아도 됨
+
+ 우리가 임의의 위치 X에 있다고 할 때:
+ X > K일 경우, 반드시 뒤로 돌아가야 함 → 시간 낭비
+ 따라서 X가 커질수록 K로 오기 위한 시간도 같이 증가
+
+ 이때, X = K + (K - N) 이상인 경우
+ 출발점 N에서 X까지 가는 시간 ≥ (K - N) + (X - K)
+ 다시 X에서 K까지 오는 데 (X - K) 시간 필요
+ 총 시간 ≥ (K - N) + 2 × (X - K)
+
+ 즉, X가 K보다 커질수록 시간 증가량은 2배 이상씩 늘어남
+ → BFS는 그런 경로를 이미 그 전에 더 짧은 거리로 도달했음
+*/
 public class N_1697 {
+    /**
+     * Previous Solve
+     * public static void main(String[] args) throws IOException {
+     * BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+     * StringTokenizer st = new StringTokenizer(br.readLine());
+     * int start = Integer.parseInt(st.nextToken());
+     * int end = Integer.parseInt(st.nextToken());
+     * if(start==end){
+     * System.out.println(0);
+     * return;
+     * }
+     * int [] map = new int[100001];
+     * <p>
+     * System.out.println(bfs(map, start, end));
+     * }
+     * public static int bfs(int[] map, int start, int end){
+     * Queue<Integer> que = new ArrayDeque<>();
+     * int [] move = new int[]{1,-1,2};
+     * <p>
+     * que.add(start);
+     * int count =0;
+     * map[start] =count;
+     * while(!que.isEmpty()){
+     * int cur =que.poll();
+     * for(int i=0;i<3;i++){
+     * int nx = (i==2)?cur*move[i]:cur+move[i];
+     * if(nx < 0 || nx>=map.length || map[nx]>0){
+     * continue;
+     * }
+     * <p>
+     * map[nx]=map[cur]+1;
+     * if(nx == end){
+     * return map[nx];
+     * }
+     * que.add(nx);
+     * }
+     * }
+     * <p>
+     * return -1;
+     * }
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
-        int end = Integer.parseInt(st.nextToken());
-        if(start==end){
+        int goal = Integer.parseInt(st.nextToken());
+        if (start == goal) {
             System.out.println(0);
             return;
         }
-        int [] map = new int[100001];
-
-        System.out.println(bfs(map, start, end));
+        System.out.println(bfs(start, goal));
     }
-    public static int bfs(int[] map, int start, int end){
-        Queue<Integer> que = new ArrayDeque<>();
-        int [] move = new int[]{1,-1,2};
 
-        que.add(start);
-        int count =0;
-        map[start] =count;
-        while(!que.isEmpty()){
-            int cur =que.poll();
-            for(int i=0;i<3;i++){
-                int nx = (i==2)?cur*move[i]:cur+move[i];
-                if(nx < 0 || nx>=map.length || map[nx]>0){
-                    continue;
-                }
+    public static int bfs(int start, int goal) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        Set<Integer> visited = new HashSet<>();
+        int[] move = {1, -1, 2};
+        queue.add(start);
+        visited.add(start);
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            count++;
+            while (len-- > 0) {
+                int curr = queue.poll();
+                for (int d = 0; d < move.length; d++) {
+                    int next;
+                    if (d == 2) {
+                        next = curr * move[d];
+                    } else next = curr + move[d];
 
-                map[nx]=map[cur]+1;
-                if(nx == end){
-                    return map[nx];
+                    if (next == goal) {
+                        return count;
+                    }
+                    if (next < 0 || next>100001|| visited.contains(next)) {
+                        continue;
+                    }
+                    queue.add(next);
+                    visited.add(next);
                 }
-                que.add(nx);
             }
         }
-
-        return -1;
+        return count;
     }
 }
