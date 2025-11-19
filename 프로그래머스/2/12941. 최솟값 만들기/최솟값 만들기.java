fd02bc7b676cf69fcf3dff3ce9f0db21d9adda75
0woy <87132052+0woy@users.noreply.github.com>
2025-11-19 20:39:17 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.99 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -2,23 +2,12 @@ import java.util.*;
 class Solution
 {    
     public int solution(int []A, int []B){
-        //정렬을 갈겨서  int af, bf, al, bl
         Arrays.sort(A);
         Arrays.sort(B);
-        int af=0;
-        int bf =0;
-        int al = A.length-1;
-        int bl = B.length-1;
-        int total =0;
-        while(af <= al && bf <= bl){
-            if(A[al] > B[bl]){
-                total += B[bl--] * A[af++];                
-            }else{
-                total += A[al--] * B[bf++];
-            }
+        int total = 0;
+        for(int i=0;i<A.length;i++){
+            total += A[i] * B[B.length-i-1];
         }
-        
-        
         return total;
     }
 }
