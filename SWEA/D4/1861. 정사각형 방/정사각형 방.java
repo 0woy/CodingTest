d6fa7e534e66844470c870ec54495f1bf280ce5e
0woy <87132052+0woy@users.noreply.github.com>
2025-11-20 23:31:38 +0900

    [D4] Title: 정사각형 방, Time: 133 ms, Memory: 35,968 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -2,63 +2,58 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Solution{
-    static boolean [][] visited;
-    final static int [] dx = {0,1,0,-1};
-    final static int [] dy = {1,0,-1,0};
-    static int max;
-    static int now;
+public class Solution {
+
+    static int[][] map;
+    static int[][] dp;
+    final static int[] dx = {0, 1, 0, -1};
+    final static int[] dy = {1, 0, -1, 0};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int tc = Integer.parseInt(br.readLine());
-        for(int t=1;t<=tc;t++){
-            sb.append("#"+t+" ");
+        for (int t = 1; t <= tc; t++) {
+            sb.append("#" + t + " ");
             int n = Integer.parseInt(br.readLine());
-            int [][] map = new int[n][n];
-            visited = new boolean[n][n];
-            max = 0;
-            int r = 0, c=0;
-            for(int i=0;i<map.length;i++){
-                int j=0;
-                for(String ch : br.readLine().split(" ")){
+            map = new int[n][n];
+            dp = new int[n][n];
+            for (int i = 0; i < map.length; i++) {
+                int j = 0;
+                for (String ch : br.readLine().split(" ")) {
                     map[i][j++] = Integer.parseInt(ch);
                 }
             }
-            for(int i=0;i<map.length;i++){
-                for(int j=0;j<map.length;j++){
-                    if(!visited[i][j]){
-                        int prev =max;
-                        now =1;
-                        dfs(map, i,j,1);
-                        if(max > prev){
-                            r = i; c =j;
-                        }else if(max == now && (r!=i || c !=j)){
-                            int cur= map[i][j];
-                            int p = map[r][c];
-                            if(cur < p) {r=i; c=j;}
-                        }
+            int bestNum = Integer.MAX_VALUE;
+            int bestLen = 0;
+            for (int i = 0; i < map.length; i++) {
+                for (int j = 0; j < map.length; j++) {
+                    int len = dfs(i, j);
+                    if (len > bestLen || (len == bestLen && map[i][j] < bestNum)) {
+                        bestLen = len;
+                        bestNum = map[i][j];
                     }
                 }
             }
-            sb.append(map[r][c]+" "+max+"\n");
+            sb.append(bestNum).append(" ").append(bestLen).append("\n");
         }
         System.out.println(sb);
     }
-    static void dfs(int [][] map, int r, int c, int count){
-        int cur = map[r][c];
-         for(int d=0;d<4;d++){
+
+    static int dfs(int r, int c) {
+        if (dp[r][c] != 0) return dp[r][c];
+        int cur= map[r][c];
+        dp[r][c]=1;
+
+        for(int d=0;d<4;d++){
             int nx = r+dx[d];
             int ny = c+dy[d];
-            if(nx < 0 || ny < 0 || nx>=map.length || ny >= map.length) continue;
-            if(!visited[nx][ny] && map[nx][ny] == cur+1){
-                visited[nx][ny] = true;
-                dfs(map, nx, ny, count+1);
-                visited[nx][ny]=false;
+            if(nx < 0 || ny <0 || nx>=map.length || ny>=map.length) continue;
+            if(map[nx][ny] == cur +1){
+                dp[r][c] = dfs(nx,ny)+1;
+                break;
             }
         }
-        max = Math.max(max, count);
-        now = Math.max(now, count);
-
+        return dp[r][c];
     }
 }
