15545ecdc0f2995bc5ad911b02ca951beb4ac6a8
0woy <87132052+0woy@users.noreply.github.com>
2025-11-20 19:36:18 +0900

    [level 2] Title: 모음 사전, Time: 17.67 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,25 +1,24 @@
-import java.util.*;
-class Solution { 
-    static int res;
-    static int lv;
-    public int solution(String word) {  
-        res= 0;
-        dfs(new StringBuilder(), word);
-        return res;
+class Solution {
+    private static final char[] VOWELS = {'A','E','I','O','U'};
+    private int count = 0;
+    private int answer = 0;
+
+    public int solution(String word) {
+        dfs("", word);
+        return answer;
     }
-    public void dfs(StringBuilder cur, String word){
-        if(cur.toString().equals(word)){
-            res = lv;
+
+    private void dfs(String cur, String target) {
+        if (cur.equals(target)) {
+            answer = count;
             return;
         }
-        if(cur.length()>=5) return;
-        for(char ch: "AEIOU".toCharArray()){
-            lv++;
-            dfs(cur.append(ch), word);
-            cur.deleteCharAt(cur.length()-1);
-            if(res!=0) return;
+        if (cur.length() == 5) return;
+
+        for (char c : VOWELS) {
+            count++;
+            dfs(cur + c, target);
+            if (answer != 0) return;
         }
-        return;
     }
-    
 }
