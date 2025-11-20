3a000a7476c8bdf9b9db58a17170bcefc2d89a50
0woy <87132052+0woy@users.noreply.github.com>
2025-11-20 13:18:46 +0900

    [level 2] Title: 프로세스, Time: 1.66 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,31 +1,25 @@
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
-            return b-a;
-        });
-        
-        Queue<Integer> que = new ArrayDeque<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<Integer> que =new ArrayDeque<>();
         for(int i=0;i<priorities.length;i++){
-            pq.add(priorities[i]); // 우선순위 삽입
-            que.add(i); // idx 삽입
+            pq.offer(priorities[i]);
+            que.offer(i);
         }
-        int num = 1;
-        while(!que.isEmpty()){
-            int curIdx = que.poll();
-            int pr = pq.peek();
-            if(priorities[curIdx]==pr){
-                if(curIdx == location) return num;
-                else {
-                 pq.poll();
-                 num++;
-                }
-            }
-            
-            que.offer(curIdx);                                   
-        }         
         
-        return num;
+        int count =1;
+        while(!que.isEmpty()){
+            int p = pq.peek();
+            int cur = que.poll();
+            if(priorities[cur]!=p) que.offer(cur);
+            else{
+                if(cur == location){ return count;}
+                else pq.poll();            
+                count++;
+                
+            }
+        }
+        return count;
     }
 }
