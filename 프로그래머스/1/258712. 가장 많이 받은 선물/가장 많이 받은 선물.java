ffb961fdfd2ddbbabec91ca27b6820454098c090
0woy <87132052+0woy@users.noreply.github.com>
2025-11-25 21:02:50 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 12.59 ms, Memory: 109 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        int n = friends.length;
+        Map<String, Integer> idx = new HashMap<>();
+        for(int i=0;i<friends.length;i++){
+            idx.put(friends[i],i);
+        }
+        
+        int [][] cnt = new int[n][n];
+        int[] give =new int[n];
+        int[] take =new int[n];
+        
+        for(String gift : gifts){
+            String [] list =gift.split(" ");
+            
+            int giver = idx.get(list[0]);
+            int taker = idx.get(list[1]);
+            
+            cnt[giver][taker]++;
+            give[giver]++;
+            take[taker]++;
+        }
+        
+        int [] giftScore =new int[n];
+        for(int i=0;i<n;i++){
+            giftScore[i]=give[i]-take[i];
+        }
+        
+        int [] score = new int[n];
+        for(int i=0;i<n;i++){
+            taker:
+            for(int j=i+1;j<n;j++){   
+                if(cnt[i][j]>cnt[j][i]) score[i]++;
+                else if(cnt[i][j] < cnt[j][i]) score[j]++;
+                else{
+                    if(giftScore[i] > giftScore[j]) score[i]++;
+                    else if(giftScore[i] < giftScore[j]) score[j]++;
+                }
+            }
+               
+        }
+        int answer = 0;
+        for(int t: score){
+            answer = Math.max(t, answer);
+        }
+        return answer;
+    }
+}
