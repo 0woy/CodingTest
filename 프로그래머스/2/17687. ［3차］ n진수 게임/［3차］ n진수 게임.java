73569fc4579e24a19eea9e8797567b27bf2d7f86
0woy <87132052+0woy@users.noreply.github.com>
2026-01-07 15:00:32 +0900

    [level 2] Title: [3차] n진수 게임, Time: 9.58 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -9,35 +9,10 @@ class Solution {
             sb.append(Integer.toString(num,n));
         }
         
-        int idx = p-1;
-        while(t>0){
-            res.append(sb.charAt(idx));
-            t--;
-            idx+=m;
+        for(int i=0;i<t;i++){
+            res.append(sb.charAt(p-1+i*m));
         }
         
         return res.toString().toUpperCase();
     }
 }
-
-/**
-2진법, 2명, 1번, 4개
-0, 1, 10, 11, 100, 101, 110, 111
-
-m*t= 2*4 = 8개 p=1;
-
-m=3, t=4, p=1 len =4*3=12
-0, 1, 10, 11, 100, 101, 110, 111
-1 2   31  23  123  123  123  123
-
-p=1: 0, 3, 6, 9
-p=2: 1, 4, 7, 10
-p=3: 2, 5, 8, 11 
-
--> startIdx = p-1 + m(m=0~<t)
-
-len = 8을 0, 2, 4, 6 / 1 3 5 7
-
-
-*///3진법
-
