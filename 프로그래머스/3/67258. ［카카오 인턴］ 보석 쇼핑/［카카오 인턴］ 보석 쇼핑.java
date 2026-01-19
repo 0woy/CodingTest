c86fa2a4e77db4bf11941572e7f234c47cf9f5b1
0woy <87132052+0woy@users.noreply.github.com>
2026-01-20 08:55:34 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 69.49 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,29 +1,25 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(String[] gems) {
-        int totalType = new HashSet<>(Arrays.asList(gems)).size();
+        int totalSize = new HashSet<>(Arrays.asList(gems)).size();
+        int [] answer = new int[2];
         Map<String, Integer> map = new HashMap<>();
-        int[] answer = new int[2];
-        int minLen = Integer.MAX_VALUE;
-        
+        int min = Integer.MAX_VALUE;
         int left =0;
-        for(int right = 0;right<gems.length;right++){
-            map.put(gems[right], 
-                    map.getOrDefault(gems[right], 0)+1);            
-            while(map.size() == totalType){
-                // System.out.println("map.size = "+map.size());
-                if(right - left < minLen){
-                    minLen = (right-left);
-                    answer[0] = left+1;
-                    answer[1] = right+1;
+        for(int right=0;right<gems.length;right++){
+            map.put(gems[right], map.getOrDefault(gems[right],0)+1);
+            while(map.size() == totalSize && left < gems.length){                
+                int len = (right-left);
+                if(len < min){
+                    min = len;   
+                    answer[0]=left+1;
+                    answer[1]=right+1;
                 }
-                
-                map.put(gems[left],map.get(gems[left])-1);
-                if(map.get(gems[left]) < 1){
-                    map.remove(gems[left]);
-                }
+                map.put(gems[left], map.get(gems[left])-1);
+                if(map.get(gems[left])==0) map.remove(gems[left]);
                 left++;
-            }
+            }            
         }
         return answer;
     }
