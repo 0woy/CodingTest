ffb7ee7f639b61ec0c5bf4a090bb08a9636cae77
0woy <87132052+0woy@users.noreply.github.com>
2026-01-23 16:57:17 +0900

    [level 3] Title: 야근 지수, Time: 126.82 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -1,21 +1,16 @@
 import java.util.*;
 class Solution {
     public long solution(int n, int[] works) {
-        long answer = Arrays.stream(works).sum();
-        if(answer <= n) return 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(
-        Collections.reverseOrder());
+        int total = Arrays.stream(works).sum();
+        if(total <= n) return 0;
+        Queue<Integer> pq =new PriorityQueue<>(Collections.reverseOrder());
+        for(int v: works) pq.offer(v);
         
-        for(int v: works){
-            pq.offer(v);
-        }
-        
-        
-        while(n > 0){
+        while(n>0){
             pq.offer(pq.poll()-1);
             n--;
         }
-        answer =0;
+        long answer = 0;
         while(!pq.isEmpty()){
             answer += Math.pow(pq.poll(),2);
         }
