6310695c0207655bf496d09214721cffc12ff36b
0woy <87132052+0woy@users.noreply.github.com>
2026-02-06 11:41:13 +0900

    [D3] Title: 햄버거 다이어트, Time: 180 ms, Memory: 33,024 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,54 +1,81 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
 
 public class Solution {
-    static class Food {
+    static class Hamburger {
         int taste;
         int kal;
 
-        public Food(int t, int k) {
-            this.taste = t;
-            this.kal = k;
+        public Hamburger(int taste, int kal) {
+            this.taste = taste;
+            this.kal = kal;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            Hamburger hamburger = (Hamburger) o;
+            return taste == hamburger.taste && kal == hamburger.kal;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(taste, kal);
         }
     }
-    static int limit;
-    static long max;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static BufferedReader br;
+    static List<Hamburger> list;
+    static int max;
+
+    public static void main(String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+
         StringBuilder sb = new StringBuilder();
         int tc = Integer.parseInt(br.readLine());
         for (int t = 1; t <= tc; t++) {
-            sb.append("#" + t + " ");
-            int[] val = Arrays.stream(br.readLine().split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-            int n = val[0];
-            limit = val[1];
+            sb.append("#").append(t).append(" ");
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int limit = Integer.parseInt(st.nextToken());
             max = 0;
-            Food[] foods = new Food[n];
-            for (int i = 0; i < foods.length; i++) {
-                int[] info = Arrays.stream(br.readLine().split(" "))
-                        .mapToInt(Integer::parseInt)
-                        .toArray();
-                foods[i] = new Food(info[0], info[1]);
-            }
-            Arrays.sort(foods, (a, b)->{
-                if(a.kal == b.kal){ return Integer.compare(b.taste, a.taste);}
-                return Integer.compare(a.kal, b.kal);
-            });
-            comb(foods, 0, 0,0);
-            sb.append(max+"\n");
+            list = new ArrayList<>();
+            initList(n);
+            maksSubSet(limit, 0, 0, 0);
+            sb.append(max + "\n");
         }
         System.out.println(sb);
     }
-    static void comb(Food[] foods, int idx, int total, int flavor){
-        max = Math.max(max, flavor);
-        for(int i=idx;i<foods.length;i++){
-            if(total+foods[i].kal <= limit)
-                comb(foods, i+1, total+foods[i].kal, flavor+foods[i].taste);
+
+    static void maksSubSet(int limit, int sum, int cur, int taste) {
+        if (limit < sum) {
+            return;
         }
+        if(cur == list.size()) {
+            max = Math.max(max, taste);
+            return;
+        }
+
+        Hamburger ham = list.get(cur);
+        maksSubSet(limit, sum + ham.kal, cur + 1, taste + ham.taste);    // 선택
+        maksSubSet(limit, sum, cur + 1, taste);  // 비선택
+    }
+
+    static void initList(int n) throws Exception {
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int d = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            list.add(new Hamburger(d, k));
+        }
+        list.sort((a, b) -> {
+            if (a.kal == b.kal) {
+                return Integer.compare(a.taste, b.taste);
+            }
+            return Integer.compare(a.kal, b.kal);
+        });
     }
 }
