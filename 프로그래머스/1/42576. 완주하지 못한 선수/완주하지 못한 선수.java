15ed2d6ace4339c8eed4e4e57a0e687ba88de3bc
0woy <87132052+0woy@users.noreply.github.com>
2026-03-20 19:35:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 116.11 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,20 +1,32 @@
 import java.util.*;
+
 class Solution {
-    public String solution(String[] participant, String[] completion) {        
-        Map<String, Integer> checkComp= new HashMap<>();
-        
-        for(String part : participant){
-            checkComp.put(part, checkComp.getOrDefault(part, 0)+1);
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+        for(String name: participant){
+            map.put(name, map.getOrDefault(name, 0)+1);
         }
         
-        for(String comp: completion){
-            checkComp.put(comp, checkComp.get(comp)-1);
-            if(checkComp.get(comp)==0){
-                checkComp.remove(comp);
-            }
+        for(String goal: completion){
+            map.put(goal, map.get(goal)-1);
+            if(map.get(goal)==0) map.remove(goal);
         }
-        
-        return checkComp.keySet().iterator().next();
-        // return "1";
+        List<String> list = new ArrayList<>(map.keySet());
+        return list.get(0);
     }
 }
+
+/**
+한 명 제외 마라톤 완주
+선수 이름 배열 p
+완주 선수 이름 배열 c
+
+미완주 선수 return 
+
+p.len = 10만 (nLogn)
+동명이인 ok = set 사용 불가
+Map <이름, 사람 수>, -> 
+1. p 돌면서 map 초기화
+2. c 돌면서 map value 감소 (0이 되면 제거)
+3. 마지막에 남은 key (사람) 반환 
+*/
