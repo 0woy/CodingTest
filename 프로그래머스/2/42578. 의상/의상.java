1efb415ee2050ffda3c0316963148fe3d02908ab
0woy <87132052+0woy@users.noreply.github.com>
2026-03-22 16:12:36 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,10 +2,10 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> map = new HashMap<>();
-        for(String [] clothe: clothes){
-            map.put(clothe[1], map.getOrDefault(clothe[1],1)+1);
+        for(String[] cloth: clothes){
+            String key = cloth[1];
+            map.put(cloth[1], map.getOrDefault(cloth[1],1)+1);
         }
-        
         int answer = 1;
         for(int v: map.values()){
             answer*=v;
@@ -13,3 +13,11 @@ class Solution {
         return answer-1;
     }
 }
+
+/**
+종류별로 한 개씩만 입을 수 있음,
+하루에 하나만 입어도 됨.
+종류: a, b, c, 0
+종류: d, e, 0
+
+*/
