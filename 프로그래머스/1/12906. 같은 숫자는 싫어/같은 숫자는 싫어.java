97d920b9537239217f734d242492df3eaa6e8882
0woy <87132052+0woy@users.noreply.github.com>
2026-03-23 19:56:25 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 45.79 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,15 +2,24 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        int prev =10;
-        List<Integer> list = new ArrayList<>();
+        Deque<Integer> stack = new ArrayDeque<>();
         for(int v: arr){
-            if(prev != v){
-                 list.add(v);
-                prev =v;
+            if(stack.isEmpty() || stack.peekLast()!=v){
+                stack.addLast(v);
             }
-            
-        }        
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        }
+        int [] answer = new int[stack.size()];
+        int i=0;
+        while(!stack.isEmpty()){
+            answer[i++] = stack.removeFirst();
+        }
+        
+        return answer;
     }
 }
+/**
+배열에서 연속적으로 나타나는 숫자 제거, 남은 수 return
+n = 백만
+숫자: 0~9
+stack -> 비어있거나 top과 숫자가 다르면 push
+*/
