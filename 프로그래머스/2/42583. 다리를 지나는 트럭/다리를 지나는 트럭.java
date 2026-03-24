819f8af407af6af169ce3f4a9af4420635e613e1
0woy <87132052+0woy@users.noreply.github.com>
2026-03-24 19:57:46 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 13.22 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,30 +1,24 @@
 import java.util.*;
-
 class Solution {
-public int solution(int bridge_length, int weight, int[] truck_weights) {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
         Queue<int[]> que = new ArrayDeque<>();
-        int time = 0;
+        int time =0;
         int totalW = 0;
-        int truckIdx = 0;
-    
-        while(truckIdx < truck_weights.length || !que.isEmpty()){
-            time++; // 시간은 기본적으로 흐름
-            if(!que.isEmpty() && que.peek()[1]==time){
-                totalW-=que.poll()[0];
-            }
-            
-            // 2. 새 트럭이 들어올 수 있는지 확인
-            if(truckIdx < truck_weights.length){
-                if(totalW+truck_weights[truckIdx] <=weight){
-                    if(que.size() < bridge_length){
-                        int arriveTime = time+bridge_length;
-                        que.offer(new int[]{truck_weights[truckIdx], arriveTime});
-                        totalW+=truck_weights[truckIdx];
-                        truckIdx++;
+        int idx=0;
+        while(idx < truck_weights.length || !que.isEmpty()){
+            time++;                        
+                
+                if(!que.isEmpty() && que.peek()[1] == time){
+                    totalW -=que.poll()[0];
+                }
+            if(idx < truck_weights.length && truck_weights[idx]+totalW<=weight){
+                    if(que.size()<bridge_length){
+                        int w =  truck_weights[idx++];
+                        totalW+=w;
+                        int arrivalTime = time+bridge_length;
+                        que.offer(new int[]{w, arrivalTime});
                     }
                 }
-            }
-                       
         }
         return time;
     }
