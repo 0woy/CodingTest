b55510a93a2479f191bc5af33929eba4e9606193
0woy <87132052+0woy@users.noreply.github.com>
2026-03-25 22:35:24 +0900

    [level 2] Title: 프로세스, Time: 0.56 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,25 +1,44 @@
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        Queue<Integer> que =new ArrayDeque<>();
+        ArrayDeque<int[]> que = new ArrayDeque<>();
+        int [] num = new int[10];
         for(int i=0;i<priorities.length;i++){
-            pq.offer(priorities[i]);
-            que.offer(i);
+            int n = priorities[i];
+            que.offer(new int[]{n,i});  // 우선순위와 인덱스
+            num[n]++; // 해당 우선순위 count+1;
         }
-        
-        int count =1;
+        int max = findMax(num);        
+        int count =0;
         while(!que.isEmpty()){
-            int p = pq.peek();
-            int cur = que.poll();
-            if(priorities[cur]!=p) que.offer(cur);
-            else{
-                if(cur == location){ return count;}
-                else pq.poll();            
+            int [] cur = que.poll();
+            if(cur[0] == max) { 
                 count++;
-                
+                num[max]--;
+                if(num[max]==0) max = findMax(num); // max 갱신
+                if(cur[1]==location) return count;
+            }else{
+                que.offer(cur);
             }
-        }
+        }        
         return count;
     }
+    static int findMax(int [] nums){
+        int max = 0;
+        for(int i=9;i>=0;i--){
+            if(nums[i]>0) return i;
+        }
+        return max;
+        
+    }
 }
+
+/**
+0~9까지 포함된 priorities 개수 count;
+현재 값이 가장 큰 값인 경우  count-1;
+
+count가 0이 된 경우, 그 다음 큰 값 찾기 (0~9까지라 ㄱㅊ)
+ArrayDeque에 삽입 삭제 반복
+
+
+*/
