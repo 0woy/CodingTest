8b57a9302723c27fb58b8ad6023da38d88756f14
0woy <87132052+0woy@users.noreply.github.com>
2026-04-02 20:59:11 +0900

    [level 2] Title: 더 맵게, Time: 1828.61 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,18 +1,40 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] scoville, int K) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int v: scoville){pq.add(v);}
-        int count =0;
-        while(pq.size()>1 && pq.peek()< K){
-            int a = pq.poll();
-            int b = pq.poll();
-            pq.offer(a+b*2);
+        for(int v: scoville) pq.offer(v);
+            
+        int count = 0;
+        while(!pq.isEmpty()){
+            int s1 = pq.poll();
+            if(s1 >= K) return count;
+            if(pq.isEmpty()) return -1;
+            int s2 = pq.poll();
+            int sum = s1+(s2*2);
+            pq.offer(sum);
             count++;
+            
         }
-        if(!pq.isEmpty() &&  pq.peek() >= K) return count;
-        
-        return -1;
+         return -1;
     }
 }
+
+/**
+K이상의 숫자로 만들기
+sum : a+(b*2)
+모든 숫자가 k 이상, 최소 횟수 return
+ n = 백만
+ k>10억 9*2 18+9 = long?
+ k이상으로 만들 수 없는 경우 -1
+ 
+ pq로 작은 값이 먼저 나오게'
+ while(!pq.isEmpty()){
+    int s1 = pq.poll();
+    if(s1 >= k) return count;
+    
+    int s2 = pq.poll();
+    int sum = s1+(s2*2);
+    pq.offer(sum);
+ }
+ return -1;
+*/
