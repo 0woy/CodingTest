a7a192cd04c3253ad5ee9b63c6b7fbc116f7d220
0woy <87132052+0woy@users.noreply.github.com>
2026-04-07 21:45:55 +0900

    [level 2] Title: 가장 큰 수, Time: 194.91 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,15 +1,35 @@
 import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
-        String[] strs= Arrays.stream(numbers)
-            .mapToObj(String::valueOf)
-            .toArray(String[]::new);
-        
-        Arrays.sort(strs, (a,b)-> (b+a).compareTo(a+b));
-        if(strs[0].equals("0")) return "0"; // 0만 여러개면 "000" → "0"
-
+        String [] nums = new String[numbers.length];        
         StringBuilder sb = new StringBuilder();
-        for(String s : strs) sb.append(s);
+        for(int i=0;i<numbers.length;i++){
+            nums[i] =String.valueOf(numbers[i]);
+        }
+        Arrays.sort(nums, (a,b)->{
+            return (b+a).compareTo(a+b);
+        });
+        for(String str: nums){
+            sb.append(str);
+        }
+        if(sb.charAt(0)=='0') return "0";
         return sb.toString();
     }
 }
+/**
+0또는 양의 정수,
+정수를 이어 붙여 만들 수 있는 가장 큰 수
+3 30 34 5 9 
+34 > 3 > 32
+
+문자로 봐야하려나?
+Map<Integer, List<String>> map
+정렬할 때는,,, List<String> tmp = map.values()
+Collections.sort(길이가 다르면, )
+345
+343
+3
+321
+300
+
+*/
